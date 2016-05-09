47e236724a211a92631d3a3817a29d4f95d12857
Zeyuan Shang <zeyuanxy@gmail.com>
2016-05-09 22:45:49 +0800

    add 47

---
+++ ninety-nine-haskell-problems/vol5/47.hs
@@ -0,0 +1,29 @@
+not' :: Bool -> Bool
+not' True = False
+not' False = True
+
+and', or', nor', nand', xor', impl', equ' :: Bool -> Bool -> Bool
+and' True True = True
+and' _ _ = False
+
+or' False False = False
+or' _ _ = True
+
+nor' a b = not' $ or' a b
+
+nand' a b = not' $ and' a b
+
+xor' True False = True
+xor' False True = True
+xor' _ _ = False
+
+impl' a b = (not' a) `or'` b
+
+equ' a b = not' (xor' a b)
+
+infixl 4 `or'`
+infixl 6 `and'`
+
+main = do
+    let value = True `and'` False
+    print value
