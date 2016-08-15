3aa3c621deb8d54e05e28122beeb27a818372dc8
Zeyuan Shang <zeyuanxy@gmail.com>
2016-08-16 00:34:55 +0800

    chap2, parse some basic types in Scheme

--- write-yourself-a-scheme/chap2/main.hs
+++ write-yourself-a-scheme/chap2/main.hs
@@ -1,23 +1,57 @@
 -- @Author: Zeyuan Shang
 -- @Date:   2016-08-13 14:00:45
 -- @Last Modified by:   Zeyuan Shang
--- @Last Modified time: 2016-08-16 00:12:35
+-- @Last Modified time: 2016-08-16 00:33:40
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
-
-symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
-readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+import Control.Monad
 
 main :: IO ()
 main = do
     (expr:_) <- getArgs
     putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value : " ++ show val
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving Show
+
+parseString :: Parser LispVal
+parseString = do 
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = [first] ++ rest
+    return $ case atom of
+                "#t" -> Bool True
+                "#f" -> Bool False
+                otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
