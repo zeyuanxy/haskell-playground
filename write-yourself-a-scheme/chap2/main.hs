d4483a803ead818df96076f5e1a6064d0ef153dc
Zeyuan Shang <zeyuanxy@gmail.com>
2016-08-16 00:14:32 +0800

    chap2, ignore spaces

--- write-yourself-a-scheme/chap2/main.hs
+++ write-yourself-a-scheme/chap2/main.hs
@@ -1,7 +1,7 @@
 -- @Author: Zeyuan Shang
 -- @Date:   2016-08-13 14:00:45
 -- @Last Modified by:   Zeyuan Shang
--- @Last Modified time: 2016-08-13 14:04:22
+-- @Last Modified time: 2016-08-16 00:12:35
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
@@ -9,8 +9,11 @@ import System.Environment
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
